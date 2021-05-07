2586862aff071927c50987223526572cb4c6ba55
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-05-07 17:04:08 -0400

    Format

--- project/app/config.py
+++ project/app/config.py
@@ -5,13 +5,10 @@ from functools import lru_cache
 from pydantic import AnyUrl, BaseSettings
 
 log = logging.getLogger("uvicorn")
-
-
 class Settings(BaseSettings):
     environment: str = os.getenv("Environment", "dev")
     testing: bool = os.getenv("Testing", 0)
     database_url: AnyUrl = os.environ.get("DATABASE_URL")
-
 
 @lru_cache()
 def get_settings() -> BaseSettings:
