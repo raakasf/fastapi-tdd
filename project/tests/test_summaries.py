1a7fae500e2e086d7c875354a8377aa2baad1447
Rakesh Sehgal <rakesh.sehgal@viasat.com>
2021-05-11 14:40:42 -0400

    Updated

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -4,12 +4,14 @@ import pytest
 
 from app.api import summaries
 
-
 def test_create_summary(test_app_with_db, monkeypatch):
     def mock_generate_summary(summary_id, url):
         return None
+
     monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
@@ -23,7 +25,7 @@ def test_create_summaries_invalid_json(test_app):
             {
                 "loc": ["body", "url"],
                 "msg": "field required",
-                "type": "value_error.missing"
+                "type": "value_error.missing",
             }
         ]
     }
@@ -36,8 +38,11 @@ def test_create_summaries_invalid_json(test_app):
 def test_read_all_summaries(test_app_with_db, monkeypatch):
     def mock_generate_summary(summary_id, url):
         return None
+
     monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get("/summaries/")
@@ -50,9 +55,12 @@ def test_read_all_summaries(test_app_with_db, monkeypatch):
 def test_read_summary(test_app_with_db, monkeypatch):
     def mock_generate_summary(summary_id, url):
         return None
+
     monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
 
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get(f"/summaries/{summary_id}")
@@ -86,6 +94,7 @@ def test_read_summary_incorrect_id(test_app_with_db):
 def test_remove_summary(test_app_with_db, monkeypatch):
     def mock_generate_summary(summary_id, url):
         return None
+
     monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -115,34 +124,64 @@ def test_remove_summary_incorrect_id(test_app_with_db):
     }
 
 
-@pytest.mark.parametrize("summary_id, payload, status_code, detail", [
-    [999, {"url": "https://foo.bar", "summary": "updated!"}, 404, "Summary not found"],
+@pytest.mark.parametrize(
+    "summary_id, payload, status_code, detail",
     [
-        0,
-        {"url": "https://foo.bar", "summary": "updated!"},
-        422,
-        [{"loc": ["path", "id"], "msg": "ensure this value is greater than 0", "type": "value_error.number.not_gt", "ctx": {"limit_value": 0}}]
-    ],
-    [
-        1,
-        {},
-        422,
         [
-            {"loc": ["body", "url"], "msg": "field required", "type": "value_error.missing"},
-            {"loc": ["body", "summary"], "msg": "field required", "type": "value_error.missing"}
-        ]
+            999,
+            {"url": "https://foo.bar", "summary": "updated!"},
+            404,
+            "Summary not found",
+        ],
+        [
+            0,
+            {"url": "https://foo.bar", "summary": "updated!"},
+            422,
+            [
+                {
+                    "loc": ["path", "id"],
+                    "msg": "ensure this value is greater than 0",
+                    "type": "value_error.number.not_gt",
+                    "ctx": {"limit_value": 0},
+                }
+            ],
+        ],
+        [
+            1,
+            {},
+            422,
+            [
+                {
+                    "loc": ["body", "url"],
+                    "msg": "field required",
+                    "type": "value_error.missing",
+                },
+                {
+                    "loc": ["body", "summary"],
+                    "msg": "field required",
+                    "type": "value_error.missing",
+                },
+            ],
+        ],
+        [
+            1,
+            {"url": "https://foo.bar"},
+            422,
+            [
+                {
+                    "loc": ["body", "summary"],
+                    "msg": "field required",
+                    "type": "value_error.missing",
+                }
+            ],
+        ],
     ],
-    [
-        1,
-        {"url": "https://foo.bar"},
-        422,
-        [{"loc": ["body", "summary"], "msg": "field required", "type": "value_error.missing"}]
-    ],
-])
-def test_update_summary_invalid(test_app_with_db, summary_id, payload, status_code, detail):
+)
+def test_update_summary_invalid(
+    test_app_with_db, summary_id, payload, status_code, detail
+):
     response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps(payload)
+        f"/summaries/{summary_id}/", data=json.dumps(payload)
     )
     assert response.status_code == status_code
     assert response.json()["detail"] == detail
@@ -151,7 +190,7 @@ def test_update_summary_invalid(test_app_with_db, summary_id, payload, status_co
 def test_update_summary_invalid_url(test_app):
     response = test_app.put(
         "/summaries/1/",
-        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
@@ -170,7 +209,7 @@ def test_update_summary(test_app_with_db, monkeypatch):
 
     response = test_app_with_db.put(
         f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 200
 
